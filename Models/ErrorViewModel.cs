ddfdb0f6e8c5b6c408e7eebde48a19dfc7640026
OMKAR <omkarprajapati060@gmail.com>
2025-07-23 14:45:33 +0530

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuickRozgar.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
